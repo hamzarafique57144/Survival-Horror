a531cddde56cc0da02988d86f7b06dbf1827f683
hamzarafique57144 <hamzarafique277@gmail.com>
2024-05-13 00:09:05 +0500

    Table and Guiding Arrow

---
+++ Assets/Scripts/Sequencing/BFirstTrigger.cs
@@ -0,0 +1,30 @@
+using StarterAssets;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BFirstTrigger : MonoBehaviour
+{
+    [SerializeField] FirstPersonController Player;
+    [SerializeField] TextMeshProUGUI TextBox;
+    [SerializeField] GameObject TheMarker;
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Player.GetComponent<FirstPersonController>().enabled = false;
+            StartCoroutine(ScenePlayer());
+        }
+       
+    }
+    IEnumerator ScenePlayer()
+    {
+        TextBox.text = "Look like a weapon on that table!";
+        yield return new WaitForSeconds(2.5f);
+        Player.GetComponent<FirstPersonController>().enabled = true;
+        TextBox.text = " ";
+        TheMarker.SetActive(true);
+
+    }
+}
